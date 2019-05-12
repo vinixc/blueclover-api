d9dbc3522a1f629b1a041095b8eb4edbefe8d5d2
Vinicius <vinicius.tecbcc@gmail.com>
2019-05-12 01:14:52 -0300

    attBanco

--- src/main/resources/db/migration/V00__criar_sequencias.sql
+++ src/main/resources/db/migration/V00__criar_sequencias.sql
@@ -2,3 +2,6 @@ create sequence codigo;
 create sequence pessoas;
 create sequence sequence_lancamento;
 create sequence cod_usuario;
+create sequence cod_permissao;
+create sequence codigo_usuario_pm;
+create sequence codigo_permissao_pm;
